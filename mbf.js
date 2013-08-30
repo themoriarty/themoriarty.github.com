106336070245fffc292ec7a2ea39f473f44a9a8f
themoriarty <m0riarty@ya.ru>
2013-08-30 00:01:06 -0700

    dnf/cnf

--- mbf.js
+++ mbf.js
@@ -84,7 +84,7 @@ function purgeOverlapping(midterms){
 	}
 	return true;
     }
-    ret = new Array();
+    var ret = new Array();
     while (!allDone(terms)){
 	console.log(terms);
 	var candidates = new Array();
@@ -113,33 +113,110 @@ function purgeOverlapping(midterms){
 }
 
 function printVar(idx, value, useXs){
-    return (value == 0 ? "\u00AC" : "") + (useXs ? "x" + idx : String.fromCharCode("A".charCodeAt(0) + idx * 1));
+    return (value == 0 ? "\u00AC" : "") + (useXs ? "x" + (idx * 1 + 1) : String.fromCharCode("A".charCodeAt(0) + idx * 1));
 }
 
-function serialize(midterms, useXs){
+function serialize(midterms, useDnf){
     var ret = "";
-    for (var i in midterms){
-	if (i != 0){
-	    ret += " + ";
-	}
-	var midterm = midterms[i];
-	for (var j in midterm.inputs){
-	    if (midterm.inputs[j] != '-'){
-		ret += printVar(j, midterm.inputs[j], useXs);
+    if (useDnf){
+	for (var i in midterms){
+	    if (i != 0){
+		ret += " + ";
 	    }
+	    var midterm = midterms[i];
+	    for (var j in midterm.inputs){
+		if (midterm.inputs[j] != '-'){
+		    ret += printVar(j, midterm.inputs[j], true);
+		}
+	    }
+	}
+    } else{
+	for (var i in midterms){
+	    var midterm = midterms[i];
+	    var term = "";
+	    for (var j in midterm.inputs){
+		if (midterm.inputs[j] != '-'){
+		    if (term.length > 0){
+			term += " + ";
+		    }
+		    term += printVar(j, !midterm.inputs[j], true);
+		}
+	    }
+	    ret += "(" + term + ")";
 	}
     }
     return ret;
 }
 
-function solve(outputs){
+function Expression(ops){
+    this.ops = ops;
+}
+Expression.prototype.toString = function(){
+    var ret = "";
+    for (var e in this.ops){
+	/*if (this.ops[e] != '*')*/{
+	    ret += this.ops[e].toString();
+	    ret += " ";
+	}
+	ret = ret.trimRight();
+    }
+    return this.ops.length > 2 ? "(" + ret + ")" : ret;
+}
+/*
+Expression.prototype.getAllMultipliers = function(){
+    for (var i in this.ops){
+	if (this.ops[i] == '+'){
+	    return new Array();
+	}
+    }
+    var op1 = this.ops[0];
+    var remainingOps = new Array();
+    for (var i = 2; i < this.ops.length; ++i){
+	remainingOps.push(this.ops[i]);
+    }
+    var ret = new Expression(remainingOps).getAllMultipliers();
+    var retLen = ret.length;
+    for (var i = 0; i < retLen; ++i){
+	var newOp = new Expression(new Array());
+	for (var j in ret[i].ops){
+	    
+	}
+    }
+}*/
+Expression.prototype.addBrackets = function(){
+    
+}
+
+function getExpression(midterms, isDnf){
+    var ops = new Array();
+    for (var i in midterms){
+	if (ops.length > 0){
+	    ops.push(isDnf ? "+" : "*");
+	}
+	var op = new Expression(new Array());
+	var midterm = midterms[i];
+	for (var j in midterm.inputs){
+	    var input = midterm.inputs[j];
+	    if (input != '-'){
+		if (op.ops.length > 0){
+		    op.ops.push(isDnf ? "*" : "+");
+		}
+		op.ops.push(((input == 1 && isDnf) || (input == 0 && !isDnf) ? "" : "\u00AC") + "x" + (j * 1 + 1));
+	    }
+	}
+	ops.push(op);
+    }
+    return new Expression(ops);
+}
+
+function solve(outputs, useDnf){
     if (Math.pow(2, Math.ceil(Math.log(outputs.length) / Math.log(2))) != outputs.length){
 	throw ("Неправильное количество выходов: " + outputs.length);
     }
     var nVars = Math.log(outputs.length) / Math.log(2);
     var midterms = new Array();
     for (var i in outputs){
-	if (outputs[i] == 1){
+	if (outputs[i] == (useDnf ? 1 : 0)){
 	    midterms.push(new Midterm([i], getInputs(i, nVars)));
 	}
     }
@@ -156,13 +233,14 @@ function solve(outputs){
     for (var i in midterms){
 	console.log(midterms[i].toString());
     }
-    console.log(serialize(midterms));
-    return midterms;
-    //document.write(serialize(midterms));
+    console.log(serialize(midterms, useDnf));
+    return getExpression(midterms, useDnf);
 }
 
 function output(text){
-    document.getElementById("output").innerText += text + "\n";
+    var p = document.createElement("p");
+    p.innerText = text;
+    document.getElementById("output").appendChild(p);
 }
 
 function main(){
@@ -172,12 +250,13 @@ function main(){
 	for (var i in text){
 	    outputs.push({'0': 0, '1': 1}[text[i]]);
 	}
-	solution = solve(outputs);
-	output("Выходы: [" + outputs.join(" ") + "], решение: " + serialize(solution) + " (или " + serialize(solution, true) + ")");
+	solutionDnf = solve(outputs, true);
+	solutionCnf = solve(outputs, false);
+	output("Выходы: [" + outputs.join(" ") + "], решение: " + solutionDnf.toString() + " или " + solutionCnf.toString());
     } catch (err){
 	output(err);
     }
 }
 //console.log(getInputs(13, 4));
-//console.log(solve([0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1]))
+//console.log(solve([0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1], true).toString());
 //console.log(solve([0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]))
